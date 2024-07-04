c30bc71439185c388fb2dfea970b14b65478b31c
Oplexitie <arthur.cottreau@gmail.com>
2024-07-04 10:25:22 +0200

    fix window layering

--- Scripts/Menu/menu_buttons.gd
+++ Scripts/Menu/menu_buttons.gd
@@ -2,9 +2,7 @@ extends Control
 
 var win_scene : NinePatchRect
 
-@export var path_office_dark : NodePath
-
-@onready var cam_stuff : Node = get_node(path_office_dark)
+@onready var ui_node : CanvasLayer = get_parent()
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -15,12 +13,12 @@ func _input(event):
 func _play_pressed():
 	if !is_instance_valid(win_scene):
 		win_scene = load("res://Scenes/Menu/window_lvlseletect.tscn").instantiate()
-		$"/root/Node2D".add_child(win_scene)
+		ui_node.add_child(win_scene)
 
 func _settings_pressed():
 	if !is_instance_valid(win_scene):
 		win_scene = load("res://Scenes/Menu/window_settings.tscn").instantiate()
-		$"/root/Node2D".add_child(win_scene)
+		ui_node.add_child(win_scene)
 
 func _quit_pressed():
 	get_tree().quit()
